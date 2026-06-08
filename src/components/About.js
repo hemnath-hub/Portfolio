6cd5f90180f19c5691f07163b56730f2870c64f5
Hemnath P <hemnathyogeshwaran@gmail.com>
2026-06-08 15:04:38 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+
+      <p>Name: Hemnath P</p>
+      <p>College: St. Joseph's College of Engineering</p>
+      <p>Department: Computer Science and Engineering</p>
+      <p>CGPA: 8.44</p>
+      <p>Location: Chennai</p>
+    </section>
+  );
+}
+
+export default About;
