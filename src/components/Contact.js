11b5b9eabd4efdb96c2102e17b61cda589155858
Hemnath P <hemnathyogeshwaran@gmail.com>
2026-06-08 15:08:14 +0530

    Add Contact component with contact information

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact Me</h2>
+
+      <p>Email: hemnathp05@gmail.com</p>
+      <p>Phone: 9345615230</p>
+      <p>Location: Chennai</p>
+    </section>
+  );
+}
+
+export default Contact;
