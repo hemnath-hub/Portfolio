5c3dafc4f5b23dfc1ce8579d1d883fe2882fb16e
Hemnath P <hemnathyogeshwaran@gmail.com>
2026-06-08 15:06:29 +0530

    Add Experience component with internship details

---
+++ src/components/Experience.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Experience.css";
+
+function Experience() {
+  return (
+    <section id="experience" className="experience">
+      <h2>Experience</h2>
+
+      <div className="card">
+        <h3>MERN Stack Developer Intern</h3>
+        <p>Codec Technologies Pvt Ltd</p>
+      </div>
+
+      <div className="card">
+        <h3>Full Stack Development Intern</h3>
+        <p>Thiranex</p>
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
